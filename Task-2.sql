1460b7dbae4cd2d067b45de2a4a2278d98313f91
artiqule <94694652+artiqule@users.noreply.github.com>
2022-07-07 18:07:41 +0300

    Update Task-2.sql

--- Task-2.sql
+++ Task-2.sql
@@ -1,18 +1,33 @@
 -- 1. Отобрать по каждому договору последнюю доступную запись в таблице				
 
-SELECT TOP 1 *
-ORDER BY DEAL_NO
-DESC LIMIT 1
+SELECT * 
+FROM DEAL d1
+WHERE d1.id IN (
+SELECT max(id) id
+FROM DEAL d
+GROUP BY deal_no)
 
 -- 2. Вывести средний баланс по каждому договору за 2017 г.			
 
 SELECT AVG(BALANCE)
-FROM T
+FROM DEAL d
 WHERE year(report_date) = '2017'
 GROUP BY report_date
 
 -- 3. Вывести суммарный баланс договоров с тарифом Зарплатный на конец 1 квартала 2018 г.
 
-SELECT sum(balance)
-FROM T
-WHERE tariff = 'Зарплатный' AND report_date >= '2018-03-31'
+SELECT deal_no, sum(balance)
+FROM DEAL d
+WHERE tariff = 'Зарплатный' AND report_date <= '2018-03-31'
+GROUP BY deal_no
+
+# Если в таблице номер договора (deal_no) повторяется, то тогда решение:
+
+SELECT deal_no, sum(balance) 
+FROM DEAL d1
+WHERE d1.id IN (
+SELECT max(id) id
+FROM DEAL d
+WHERE tariff = 'Зарплатный' AND report_date < '2018-04-01'
+GROUP BY deal_no)
+GROUP BY deal_no
